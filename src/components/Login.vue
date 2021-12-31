962aafe89ff176f7161b12ef2a845cf4e25ed255
LoverITer <2489868503@qq.com>
2021-12-31 23:31:41 +0800

    feat: 完成login box的绘制

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,6 +4,23 @@
       <div class="avatar-box">
         <img src="../assets/logo.png">
       </div>
+      <!--登录表单-->
+      <el-form class="login-form">
+        <!---用户名-->
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+
+        <!--密码-->
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -13,11 +30,12 @@ export default {}
 </script>
 
 <style scoped>
-.login-container{
+.login-container {
   background-color: #2b4b6b;
   height: 100%;
 }
-.login-box{
+
+.login-box {
   width: 450px;
   height: 300px;
   background-color: #fff;
@@ -25,10 +43,10 @@ export default {}
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
 }
 
-.avatar-box{
+.avatar-box {
   height: 130px;
   width: 130px;
   border: 1px solid #eee;
@@ -37,13 +55,27 @@ export default {}
   box-shadow: 0 0 10px #ddd;
   position: absolute;
   left: 50%;
-  transform: translate(-50%,-40%);
+  transform: translate(-50%, -40%);
   background-color: #fff;
 }
-.avatar-box img{
+
+.avatar-box img {
   width: 100%;
   height: 100%;
   border-radius: 50%;
   background-color: #eee;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.login-form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 </style>
