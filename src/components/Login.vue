fe1fc5671882ff5c12059b7735dac74b04e7a897
LoverITer <2489868503@qq.com>
2021-12-31 23:17:31 +0800

    feat: 完成login box的框架绘制

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <div class="avatar-box">
+        <img src="../assets/logo.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.login-container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login-box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+
+.avatar-box{
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%,-40%);
+  background-color: #fff;
+}
+.avatar-box img{
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #eee;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import index from './router'
 import './plugins/element.js'
+import './assets/global.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+  router: index,
+  render: c => c(App)
 }).$mount('#app')
