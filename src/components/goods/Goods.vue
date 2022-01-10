945ca6a5b5981e58c3a242523538ebd79fb1f847
LoverITer <2489868503@qq.com>
2022-01-10 22:01:06 +0800

    feat: 初始化商品列表组件

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    商品列表组件
+    <!--面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item to="/goods">商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card>
+
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Goods'
+  name: 'Goods',
+  data () {
+    return {}
+  },
+  methods: {},
+  created () {
+  }
 }
 </script>
 
