d7b6e3e96b1461adbcd169749927fbd011466359
Huangxin <52988156+LoverITer@users.noreply.github.com>
2022-01-22 15:46:51 +0800

    Dev orders (#7)
    * feat: 商品分类组件初始化
    
    * feat: 商品管理组件及路由初始化
    
    * feat: 商品分类树形表格级添加表单级联选择器渲染
    
    * feat: 完成商品分类添加
    
    * feat: 分类参数组件开发
    
    * feat: 分类参数组件tag
    
    * feat: 分类参数组件参数项修改和保存
    
    * feat: 分类参数组件参数项修改和保存
    
    * feat: 初始化商品列表组件
    
    * feat: 商品列表组件完成表格基本渲染以及分页功能
    
    * feat: 商品添加组件开发
    
    * feat: 订单列表渲染
    
    * feat: 订单列表渲染
    
    * feat: 使用echarts渲染图表
    
    * feat: 上线优化配置
    
    * feat: 上线优化配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  //自定义不同环境下得打包入口
+  chainWebpack: config => {
+
+    //发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+    })
+
+    //开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
 }
